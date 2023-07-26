9f147dcaccd680815339f8d177b5781f12adcbe6
chienheng <harry1995841025@gmail.com>
2023-07-26 21:32:08 +0800

    EP05 half

---
+++ Assets/Script/Utils/Heap.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Heap<T> where T : IHeapItem<T>
+{
+    T[] items;
+    int currentItemCount;
+
+    public Heap(int maxHeapSize)
+    {
+        items = new T[maxHeapSize];
+    }
+
+    public void Add(T item)
+    {
+        item.HeapIndex = currentItemCount;
+        items[currentItemCount] = item;
+        SortUp(item);
+        currentItemCount++;
+    }
+
+    public T RemoveFirst()
+    {
+        T firstItem = items[0];
+        currentItemCount--;
+        items[0] = items[currentItemCount];
+        items[0].HeapIndex = 0;
+        SortDown(items[0]);
+        return firstItem;
+    }
+
+    public int Count { get{ return currentItemCount; } }
+    public bool Contains( T item)
+    {
+        return Equals(items[item.HeapIndex], item);
+    }
+
+    void SortUp(T item)
+    {
+        int parentIndex = (item.HeapIndex - 1) / 2;
+
+        while (true)
+        {
+            T parentItem = items[parentIndex];
+            if(item.CompareTo(parentItem) > 0)
+            {
+                Swap(item, parentItem);
+            }
+            else
+            {
+                break;
+            }
+            parentItem = items[parentIndex];
+        }
+    }
+
+    void SortDown(T item)
+    {
+        while (true)
+        {
+            int childIndexLeft = item.HeapIndex * 2 + 1;
+            int childIndexRight = item.HeapIndex * 2 + 2;
+            int swapIndex = 0;
+
+            // check child node exist 
+            if(childIndexLeft < currentItemCount)
+            {
+                swapIndex = childIndexLeft;
+
+                if(childIndexRight < currentItemCount)
+                {
+                    // default is left,thus when priority of right > left,should change index
+                    if(items[childIndexLeft].CompareTo(items[childIndexRight]) < 0)
+                    {
+                        swapIndex = childIndexRight;
+                    }
+                }
+
+                if (item.CompareTo(items[swapIndex]) < 0)
+                {
+                    Swap(item, items[swapIndex]);
+                }
+                else { return; }
+            }
+            else { return; }
+        }
+    }
+
+    // itemA and itemB must different
+    // ref: https://dotblogs.com.tw/abbee/2010/09/27/17910
+    void Swap(T itemA, T itemB)
+    {
+        int indexA = itemA.HeapIndex;
+        int indexB = itemB.HeapIndex;
+        indexA ^= indexB;
+        indexB ^= indexA;
+        indexA ^= indexB;
+    }
+
+}
+
+public interface IHeapItem<T>:IComparable<T>
+{
+    int HeapIndex { get; set; }
+
+}
